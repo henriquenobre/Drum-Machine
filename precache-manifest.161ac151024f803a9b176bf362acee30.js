7b175bc838dda167c49862aee27299cbc372fcb9
Henrique <henriquenobr@gmail.com>
2021-11-22 16:46:32 -0300

    Updates

---
+++ precache-manifest.161ac151024f803a9b176bf362acee30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a649ac8242e2e142669",
+    "url": "/Drum-Machine/static/css/main.b3600e9c.chunk.css"
+  },
+  {
+    "revision": "4a649ac8242e2e142669",
+    "url": "/Drum-Machine/static/js/main.2716dd0a.chunk.js"
+  },
+  {
+    "revision": "5b40f0d425fc9f654e0e",
+    "url": "/Drum-Machine/static/js/runtime~main.2707ea63.js"
+  },
+  {
+    "revision": "7d1851d81603a87ff103",
+    "url": "/Drum-Machine/static/js/2.ea4d1651.chunk.js"
+  },
+  {
+    "revision": "d1c26b036d206e535b041a556b732bee",
+    "url": "/Drum-Machine/index.html"
+  }
+];
